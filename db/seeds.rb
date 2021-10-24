652c8254a1b41efd3bb5d64250664c3dc42bdde8
Brinton <brintonfoyreed@gmail.com>
2021-10-23 19:21:28 -0700

    Deleted migrations; will add using rails resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# require "faker" gem
+
+User.delete_all
+Story.delete_all
+Word.delete_all
+
+brinton = User.create(name: "brinton")
+willow = User.create(name: "willow")
+
+clock = Word.create(word: "clock")
+jump = Word.create(word: "jump")
+ran = Word.create(word: "ran")
+moscow = Word.create(word: "moscow")
+july = Word.create(word: "july")
+
+puts "SEEDED THE DATABASE"
